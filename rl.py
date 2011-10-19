e340966b7df5e8f8b0ba520267f8aaf4aca08101
Joakim Carselind <j.carselind@gmail.com>
2011-10-19 17:51:05 +0200

    Added printing

--- rl.py
+++ rl.py
@@ -4,34 +4,6 @@ import pylab
 import numpy, random
 import animate
 
-gamma = 0.9
-
-'''
-Binary representation of each state. 
-Not used at the moment
-'''
-states = (
-	(0,0,0,0),
-	(1,0,0,0),
-	(0,1,0,0),
-	(1,1,0,0),
-
-	(0,0,1,0),
-	(1,0,1,0),
-	(0,1,1,0),
-	(1,1,1,0),
-
-	(0,0,0,1),
-	(1,0,0,1),
-	(0,1,0,1),
-	(1,1,0,1),
-
-	(0,0,1,1),
-	(1,0,1,1),
-	(0,1,1,1),
-	(1,1,1,1)
-)
-
 '''
 This is the transition matrix delta. At state s and action a,
 trans(s,a) = s' = next state
@@ -59,7 +31,7 @@ trans = (
 Reward matrix that makes it walk according to our opinion and view of walking
 This works better for policy iteration. Is to specific in its reward
 '''
-rew_walks = (
+rew1 = (
 	(1, 0, 0, 0) , #0
 	(0, 1, 0, 0) , #1
 	(1, 0, 0, 0) ,#2
@@ -76,6 +48,28 @@ rew_walks = (
 	(0, 1, 0 , 0), #13
 	(0, 0, 0 , 1), #14
 	(0 , 0 , 0, 0), #15
+)
+'''
+This rewards matrix has negative values for moving its leg
+backwards when it is in the upright position
+'''
+rew2 = (
+	(1, 0, 0, 0) , #0
+	(0, 0, 0, 0) , #1
+	(0, -1, 0, 0) ,#2
+	(0, 0, 1, 0) , #3
+	(0, 0, 0, 0) , #4
+	(0, 0, 0, 0) ,#5
+	(0, 0, 0 , 0), #6
+	(0, 0, 0 , 0), #7
+	(0, 0, 0, -1), #8
+	(0, 0, 0, 0), #9
+	(0, 0 ,0 , 0), #10
+	(0, 1, 0, 0), #11
+	(1, 0, 0, 0), #12
+	(0, 1, 0, 0), #13
+	(0, 0, 0, 1), #14
+	(0, 0, 0, 0), #15
 )
 
 '''
@@ -83,67 +77,76 @@ This is the reward matrix 16x4. For each state s and action a,
 rew(s,a) = r = reward value for doing a in s
 '''
 rew = (
-	(1, 0, 0, 0) , #0
+	(0, 0, 0, 0) , #0
 	(0, 1, 0, 0) , #1
 	(1, 0, 0, 0) ,#2
-	(0, 0, 1, 0) , #3
-	(0, 0, 0, 0) , #4
+	(0, 0, 0, 0) , #3
+	(0, 0, 0, 1) , #4
 	(0, 0, 0, 0) ,#5
-	(0, 0, 0 , 0), #6
-	(0, 0, 0 , 0), #7
+	(0, 0, 0, 0), #6
+	(0, 0, 0, 0), #7
 	(0, 0, 1, 0), #8
 	(0, 0, 0, 0), #9
 	(0, 0 ,0 , 0), #10
-	(0, 1, 0, 0), #11
-	(1, 0, 0, 0), #12
-	(0, 1, 0 , 0), #13
-	(0, 0, 0 , 1), #14
-	(0 , 0 , 0, 0), #15
+	(0, 0, 0, 0), #11z
+	(0, 0, 0, 0), #12
+	(0, 1, 0, 0), #13
+	(0, 0, 0, 1), #14
+	(0, 0, 0, 0), #15
 )
 
 policy = [ None for s in trans ]
 value = [ 0 for s in trans ]
+def policyIteration(rew, gamma, iterations):
+	global policy, value, trans
 
-def argmax(f, args):
-	mi = None
-	m = -1*pow(10,-10)
-	for i in args:
-		v = f(i)
-		if v > m:
-			m = v
-			mi = i
-	return mi
-
-def policyIteration():
-	global gamma, policy, value, trans, rew
+	def argmax(f, args):
+		mi = None
+		m = -1*pow(10,-10)
+		for i in args:
+			v = f(i)
+			if v > m:
+				m = v
+				mi = i
+		return mi
 	
-	for s in range(len(policy)):
-		policy[s] = argmax( lambda a: rew[s][a]+gamma*value[trans[s][a]], range(4))
-	for s in range(len(value)):
-		a = policy[s]
-		value[s] = rew[s][a]+gamma*value[trans[s][a]]
+	# Policy iteration step
+	for p in range(iterations):	
+		for s in range(len(policy)):
+			policy[s] = argmax( lambda a: rew[s][a]+gamma*value[trans[s][a]], range(4))
+		for s in range(len(value)):
+			a = policy[s]
+			value[s] = rew[s][a]+gamma*value[trans[s][a]]
 
-for p in range(100):
-	policyIteration()
-	#print policy, 'total value:', sum(value)
+def testPolicyIteration(rew, gamma, iterations):
+	print '** Testing policy iteration algorithm **'
+	print 'Gamma: ', gamma
+	print 'Iterations: ', iterations
+	policyIteration(rew, gamma, iterations)
 
+	print '\tOptimal policy: ', policy
+	optimal_state_map = [ trans[a][policy[a]] for a in range(len(policy)) ]    
+	print '\tOptimal state map', optimal_state_map
+	
+	current_state = int(random.random()*len(policy))
+	path = [current_state]
+	for i in range(20):
+		current_state = optimal_state_map[current_state]
+		path += [current_state]
 
-print 'Optimal policy: ', policy
-optimal_state_map = [ trans[a][policy[a]] for a in range(len(policy)) ]    
-print optimal_state_map
+	print '\t(Hopefully) correct walking path', path
+	
+	animate.draw(path)
 
-current_state = int(random.random()*len(policy))
-path = [current_state]
-for i in range(20):
-	current_state = optimal_state_map[current_state]
-	path += [current_state]
+gamma = 0.9
+iterations = 100
+testPolicyIteration(rew, gamma, iterations)
 
-print path
-#animate.draw(path)
-
+################
 ## Q-LEARNING ##
+################
 
-# Change the reward matrix to include less cred than for temporal learning
+"The transition and rewards are 'hidden' for the Q-learning algorithm"
 class Environment():
 	def __init__(self, trans, rew, state = 0):
 		self.state = state
@@ -155,19 +158,13 @@ class Environment():
 		self.state = self.trans[self.state][action]
 		return self.state, r
 
-
-Q = [ [random.random()*0.5 for a in range(4)] for s in range(16)]
-
-epsilon = 0.01
-surprise = 0.5
-s = random.choice(range(16))
-state_sequence = []
-
-def qLearning():
-	global s, gamma, epsilon, surprise, state_sequence, trans, rew
+def qLearning(gamma, epsilon, surprise):
+	global state_sequence, trans, rew
+	s = random.choice(range(16))
 	environment = Environment(trans, rew, s)
+	Q = [ [random.random()*0.5 for a in range(4)] for s in range(16)]
 
-	for step in range(100000):
+	for step in range(10000):
 		action_list = Q[s]
 		if random.random() < epsilon:
 			a = random.choice(range(len(action_list)))
@@ -183,7 +180,21 @@ def qLearning():
 		state_sequence += [s]
 		s = s_next
 
-qLearning()
-l = len(state_sequence)
-print state_sequence[l-20:]
-animate.draw(state_sequence[l-20:])
+def testQLearning(gamma, epsilon, surprise):
+	global state_sequence
+	print '** Testing Q-Learning algorithm **'
+	print 'Gamma: ', gamma
+	print 'Epsilon: ', epsilon
+	print 'Suprise: ', surprise
+
+	qLearning(gamma, epsilon, surprise)
+	
+	l = len(state_sequence)
+	print '\tState sequence ', state_sequence[l-20:]
+	animate.draw(state_sequence[l-20:])
+
+gamma = 0.9
+epsilon = 0.20
+surprise = 0.9
+state_sequence = []
+testQLearning(gamma, epsilon, surprise)
